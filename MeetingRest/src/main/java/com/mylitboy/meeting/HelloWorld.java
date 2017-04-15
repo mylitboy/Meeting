9c2195358c2acc8918af549b6b866f5488c7c4d7
mylitboy <mylitboy@163.com>
2017-04-15 10:22:13 +0800

    Add Web Support

---
+++ MeetingRest/src/main/java/com/mylitboy/meeting/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mylitboy.meeting;
+
+/**
+ * Created by mylitboy on 2017-4-15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
